ea7a8334400ebe186d2ffe3faf6a064dbe2a01a3
sven-n <sven-n@users.noreply.github.com>
2023-06-01 14:40:15 +0200

    example works now

--- src/python/service.py
+++ src/python/service.py
@@ -5,49 +5,56 @@ import json
 
 hostName = "localhost"
 serverPort = 5000
+endpoint_path = "/get-triples"
 tripleGenerator = TripleGenerator()
+default_encoding = "utf-8"
 
 class MyRequestHandler(BaseHTTPRequestHandler):
+    
+    def handle_triples_request(self):
+        # Body mit dem Text einlesen:
+        content_len = int(self.headers.get('Content-Length'))
+        print("Received request with %s bytes", content_len)
+        body_bytes = self.rfile.read(content_len)
+        body_string = decode(body_bytes, default_encoding)
+
+        # Tripel erzeugen:
+        triples = tripleGenerator.generate(body_string)
+        print("Generated %s triples" % len(triples))
+
+        # HTTP Header:
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+        
+        # Tripel als JSON in den Body schreiben:
+        triples_serialized = json.dumps(triples, default=vars)
+        result_bytes = bytes(triples_serialized, default_encoding)
+        self.wfile.write(result_bytes)
+        print("Wrote %s bytes as response" % len(result_bytes))
+
     def do_GET(self):
         print("GET received")
+        self.send_response(400)
+        self.end_headers()
+        self.wfile.write(bytes("Wrong HTTP Method. The service supports expects POST instead of GET.", default_encoding))
+
     def do_POST(self):
         print("POST received")
-        self.send_response(501)
-        self.end_headers()
-        return
-        
-        if self.path == '/get-triples':
-
-            # Body mit dem Text einlesen:
-            content_len = int(self.headers.get('Content-Length'))
-            print("Received request with %s bytes", content_len)
-            self.headers.get('Content-Length')
-            body_bytes = self.rfile.read(content_len)
-            body_string = decode(body_bytes, "utf-8")
-
-            # Tripel erzeugen:
-            triples = tripleGenerator.generate(body_string)
-            print("Generated %s triples", triples.length)
-
-            # HTTP Header:
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers()
-            
-            # Tripel als JSON in den Body schreiben:
-            triples_serialized = json.dumps(triples)
-            result_bytes = bytes(triples_serialized, "utf-8")
-            self.wfile.write(result_bytes)
-            print("Wrote %s bytes as response", result_bytes.length)
+        if self.path == endpoint_path:
+             self.handle_triples_request()
         else:
-            print("unknown URI: %s", self.path)
-            print("expected URI: /get-triples")
-            self.send_response(500)
+            self.send_response(400)
             self.end_headers()
+            self.wfile.write(bytes("Wrong URI. Check console output for the expected URI", default_encoding))
+            print("Wrong URI. Expected: 'http://%s:%s%s'. The text is passed in the body of the POST request." % (hostName, serverPort, endpoint_path))
+        
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyRequestHandler)
     print("Server started http://%s:%s" % (hostName, serverPort))
+
+    print("Example request: POST 'http://%s:%s%s' with the text content written into the body with UTF-8 encoding." % (hostName, serverPort, endpoint_path))
 
     try:
         webServer.serve_forever()
--- src/python/triplegen.py
+++ src/python/triplegen.py
@@ -2,7 +2,7 @@ from triple import Triple
 
 class TripleGenerator:
     # Generates relationship-triples based on an input text.
-    def generate(inputText: str) -> list[Triple]:
+    def generate(self, inputText: str) -> list[Triple]:
 
         # BIG TODO: Analyze text (with spaCy) and return the result
 
