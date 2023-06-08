3c8e490954de1e42ac2ea773e1368e9e14269f42
Adriana Mikuteit <adriana.kozlowski@web.de>
2023-06-08 22:53:19 +0200

    Modell wurde ergänzt

--- src/python/triplegen.py
+++ src/python/triplegen.py
@@ -1,14 +1,14 @@
 from triple import Triple
+from model import Model
 
 class TripleGenerator:
     # Generates relationship-triples based on an input text.
     def generate(self, inputText: str) -> list[Triple]:
-
         # BIG TODO: Analyze text (with spaCy) and return the result
+        token = Model.runNLP(inputText[0])
 
         exampleResult = [
-            Triple('sun', 'be', 'star'),
-            Triple('star', 'be', 'bright'),
+            Triple(token[0].text, 'child', token[1].text) # Relation muss angepasst werden. In der Relation werden auch die Entitäten gespeichert
         ]
 
         return exampleResult
