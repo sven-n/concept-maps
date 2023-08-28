d74db6b6bb2172e73ddccbec55b104940aa45c95
sven-n <sven-n@users.noreply.github.com>
2023-08-28 19:28:12 +0200

    Fehlende Kommentare ergänzt

--- src/ConceptMaps.UI/Components/Model.razor.cs
+++ src/ConceptMaps.UI/Components/Model.razor.cs
@@ -3,6 +3,9 @@
 using ConceptMaps.UI.Services;
 using Microsoft.AspNetCore.Components;
 
+/// <summary>
+/// Component to display and activate a trained NLP model.
+/// </summary>
 public partial class Model
 {
     /// <summary>
@@ -11,11 +14,20 @@ public partial class Model
     [Inject]
     private IModelProvider ModelProvider { get; set; } = null!;
 
+    /// <summary>
+    /// Gets or sets the name of the model.
+    /// </summary>
     [Parameter]
     public string ModelName { get; set; }
 
+    /// <summary>
+    /// Gets or sets the type of the model.
+    /// </summary>
     [Parameter]
     public ModelType ModelType { get; set; }
 
+    /// <summary>
+    /// Gets a value indicating whether this model is active.
+    /// </summary>
     private bool IsActive => this.ModelProvider.IsActiveModel(this.ModelType, this.ModelName);
 }
